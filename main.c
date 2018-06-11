356476098032cc937210afb785387d1eee339446
manisreenivas <39898803+manisreenivas@users.noreply.github.com>
2018-06-11 22:25:29 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "stm32f7xx.h"                  // Device header
+
+
+//we have to implement the watchdog interrupt handler 
+
+void WWDG_IRQHandler(void)
+{
+	for(int i=0 ; i < 50 ; i++);
+	
+}
+
+void generate_interrupt(void)
+{
+	//Lets simulate the watchdog interrupt
+	NVIC_EnableIRQ(WWDG_IRQn);
+	NVIC_SetPendingIRQ(WWDG_IRQn);
+}
+
+int main(void)
+{
+  	generate_interrupt();
+  	while(1);
+ 	return 0;
+}
+
